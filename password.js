7904baa82ceb73819362ea68717d3441c6fb6b9b
vinzz076 <ahmadfaridwahyu@gmail.com>
2025-05-31 06:25:26 +0700

    Add files via upload

---
+++ password.js
@@ -0,0 +1,17 @@
+{
+
+  "secretCode": "Vinzz",
+
+  "description": "Nero Multi Device", 
+
+  "lastUpdated": "31/05/2025",
+
+  "author": "VinzzDev", 
+
+  "status": "Active",
+
+  "license": "MIT",
+
+  "version": "1.0"
+
+    }
